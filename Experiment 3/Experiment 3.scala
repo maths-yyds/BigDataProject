9cb0773cdc2fe783c65db1c3075bc4058ae5e012
Jingwei <1363372725@qq.com>
2022-06-25 18:27:29 +0200

    Code

---
+++ Experiment 3/Experiment 3.scala
@@ -0,0 +1,17 @@
+package org.rubigdata
+
+import org.apache.spark.sql.SparkSession
+import org.apache.hadoop.io.NullWritable
+import de.l3s.concatgz.io.warc.{WarcGzInputFormat,WarcWritable}
+import de.l3s.concatgz.data.WarcRecord
+import org.apache.spark.SparkConf
+import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document,Element}
+import collection.JavaConverters._
+import java.net.URI
+
+object RUBigDataApp {
+  def main(args: Array[String]) {
+
+  }
+}
